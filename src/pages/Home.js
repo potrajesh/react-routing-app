aa2b555fbb52a60144ba1dc62c325279b8af9abf
potrajesh <raj5823p@gmail.com>
2024-12-04 19:21:50 +0530

    routing example

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+/* import React from "react";
+
+const Home = () => {
+  return <h1>Welcome to the Home Page!</h1>;
+};
+
+export default Home;
+ */
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      home
+    </div>
+  )
+}
